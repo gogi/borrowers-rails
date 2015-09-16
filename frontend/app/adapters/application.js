4fccb71ffff9050a484d2c924079f6825b3026ba
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-09-16 21:28:39 +0200

    Add host with external api to ember app

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -2,6 +2,7 @@ import ActiveModelAdapter from'active-model-adapter';
 import $ from 'jquery';
 
 export default ActiveModelAdapter.extend({
+  host: 'http://api.ember-cli-101.com',
   namespace: 'api',
   headers: {
     "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
