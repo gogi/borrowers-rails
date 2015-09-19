b6ce060dea9b7805577bc2d2a12555d6de5e202e
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-09-19 18:35:36 +0200

    Create button that deletes friends from index

--- frontend/app/routes/friends/index.js
+++ frontend/app/routes/friends/index.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
   }
+  actions: {
+    delete(friend) {
+      friend.destroyRecord();
+      return false;
+    }
+  }
 });
