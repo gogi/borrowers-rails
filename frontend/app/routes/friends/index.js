36a01fac4bf97774be21e576dc22440921a2f49a
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-09-19 18:40:30 +0200

    Insert missing coma before actions

--- frontend/app/routes/friends/index.js
+++ frontend/app/routes/friends/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
-  }
+  },
   actions: {
     delete(friend) {
       friend.destroyRecord();
