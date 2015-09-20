ed3c4f68db3bab7e6b6ea1bf198be550b55ef332
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-09-20 19:31:26 +0200

    Create API v2 from sideload serializer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     resources :articles, except: [:new, :edit]
     resources :friends, except: [:new, :edit]
+    namespace :v2 do
+      resources :friends,  except: [:new, :edit]
+      resources :articles, except: [:new, :edit]
+    end
   end
 
   get '/*path' => 'application#index'
