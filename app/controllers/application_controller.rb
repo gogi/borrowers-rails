01ab82a17e2fa29cd2869d9e8943a531f93a7ecd
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-10-20 23:48:21 +0200

    Fix loading appliaction by removing render index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-    render :index
+  def indexS
   end
 end
