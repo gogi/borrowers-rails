22d231fb33204f7116387e1655f8297c0243acb2
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-10-21 18:57:42 +0200

    Remove unsaved changes from friends #index route

--- frontend/app/routes/friends/index.js
+++ frontend/app/routes/friends/index.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
   },
+  resetController(controller, isExisting) {
+    if (isExisting) {
+      var model = controller.get('model');
+      model.rollback();
+    }
+  },
 });
