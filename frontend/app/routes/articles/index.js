1eafccdd30ce0177c790a24f3ad510320a440d3d
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-10-26 21:02:25 +0100

    Add resetController action to articles index to clear unsaved records from store

--- frontend/app/routes/articles/index.js
+++ frontend/app/routes/articles/index.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.modelFor('friends/show').get('articles');
   },
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      var model = controller.get('model');
+      model.rollback();
+    }
+  },
   actions: {
     save(model) {
       model.save();
