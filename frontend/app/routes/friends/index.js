4458db7d1d242f3025d56abfdbf22cdfb1bfb8d6
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-10-26 20:58:46 +0100

    Undo resetController function for friends index

--- frontend/app/routes/friends/index.js
+++ frontend/app/routes/friends/index.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
-  },
-  resetController(controller, isExisting) {
-    if (isExisting) {
-      var model = controller.get('model');
-      model.rollback();
-    }
-  },
+  }
 });
