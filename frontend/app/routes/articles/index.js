43d726fc9ce14a39668c48140f61a785d366e41c
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-10-26 21:52:02 +0100

    Remove brackets after rollback ends ember error type

--- frontend/app/routes/articles/index.js
+++ frontend/app/routes/articles/index.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
   resetController(controller, isExiting) {
     if (isExiting) {
       var model = controller.get('model');
-      model.rollback();
+      model.rollback;
     }
   },
   actions: {
